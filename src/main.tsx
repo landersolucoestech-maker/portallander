0ec4ba53ed7815b205ae65c8c185cb5e3bb55cf5
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-27 18:49:39 -0300

    feat: activate editable public hero runtime

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import App from './App'
+import App from './PortalApp'
 import './styles.css'
 import './public.css'
 import './public-logo.css'
 import './public-tweaks.css'
+import './public-mockup.css'
+import './hero-editable.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

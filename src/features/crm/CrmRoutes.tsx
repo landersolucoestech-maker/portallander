9a65b1f3a13614bb37258888db7e12f51bb76565
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-28 23:29:44 -0300

    refactor(crm): route extracted operation pages

--- src/features/crm/CrmRoutes.tsx
+++ src/features/crm/CrmRoutes.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { CampaignsPage, FinancePage, ReportsPage } from './CrmOperations'
 import { ActivitiesPage } from './pages/ActivitiesPage'
+import { CampaignsPage } from './pages/CampaignsPage'
 import { ContactsPage } from './pages/ContactsPage'
 import { CrmDashboardPage } from './pages/CrmDashboardPage'
 import { DealsPage } from './pages/DealsPage'
+import { FinancePage } from './pages/FinancePage'
 import { PipelinePage } from './pages/PipelinePage'
+import { ReportsPage } from './pages/ReportsPage'
 
 export default function CrmRoutes(){
   return <Routes>

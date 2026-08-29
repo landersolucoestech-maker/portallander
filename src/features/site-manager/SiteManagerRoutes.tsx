eba12856c50f568b59cf5161151241a035ec08c7
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-28 23:26:24 -0300

    refactor(site-manager): route extracted catalog pages

--- src/features/site-manager/SiteManagerRoutes.tsx
+++ src/features/site-manager/SiteManagerRoutes.tsx
@@ -1,8 +1,9 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import HeroManagerPage from './HeroManagerPage'
 import { SiteContents, SiteManagerDashboard, SitePages } from './SiteManagerWorkspace'
-import { SiteCategories, SiteMedia } from './SiteManagerCatalogPages'
 import { MediaKitPage, SiteSettingsPage } from './SiteManagerOperations'
+import { SiteCategoriesPage } from './pages/SiteCategoriesPage'
+import { SiteMediaPage } from './pages/SiteMediaPage'
 
 export default function SiteManagerRoutes(){
   return <Routes>
@@ -10,8 +11,8 @@ export default function SiteManagerRoutes(){
     <Route path="/app/site/home/hero" element={<HeroManagerPage/>}/>
     <Route path="/app/site/conteudos" element={<SiteContents/>}/>
     <Route path="/app/site/paginas" element={<SitePages/>}/>
-    <Route path="/app/site/categorias" element={<SiteCategories/>}/>
-    <Route path="/app/site/midia" element={<SiteMedia/>}/>
+    <Route path="/app/site/categorias" element={<SiteCategoriesPage/>}/>
+    <Route path="/app/site/midia" element={<SiteMediaPage/>}/>
     <Route path="/app/site/midia-kit" element={<MediaKitPage/>}/>
     <Route path="/app/site/configuracoes" element={<SiteSettingsPage/>}/>
     <Route path="*" element={<Navigate to="/app/site" replace/>}/>

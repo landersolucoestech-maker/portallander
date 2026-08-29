3ef0b052043f3665b7f43da8bb6ffef42008fb39
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 15:52:46 -0300

    refactor: remove crm and integrations routes

--- src/app/InternalApp.tsx
+++ src/app/InternalApp.tsx
@@ -1,7 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../features/access/LoginPage'
 import { WorkspacePage } from '../features/access/WorkspacePage'
-import CrmRoutes from '../features/crm/CrmRoutes'
 import SiteManagerRoutes from '../features/site-manager/SiteManagerRoutes'
 import '../styles/admin-entry.css'
 
@@ -10,7 +9,6 @@ export default function InternalApp(){
     <Route path="/app" element={<Navigate to="/app/login" replace/>}/>
     <Route path="/app/login" element={<LoginPage/>}/>
     <Route path="/app/workspaces" element={<WorkspacePage/>}/>
-    <Route path="/app/crm/*" element={<CrmRoutes/>}/>
     <Route path="/app/site/*" element={<SiteManagerRoutes/>}/>
     <Route path="*" element={<Navigate to="/app/login" replace/>}/>
   </Routes>

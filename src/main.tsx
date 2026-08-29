7222980fe418fdeb155fbdaacc63ee6bf6a4f56c
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-28 23:46:20 -0300

    refactor(home): remove declarative home bridges from bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './app/PortalApp'
-import { HomeAdBridge } from './pages/home/components/HomeAdBridge'
 import { HeaderBrandBridge } from './shared/branding/components/HeaderBrandBridge'
 import { BrandAssetsBridge } from './shared/branding/components/BrandAssetsBridge'
 import { NewsAdBridge } from './pages/noticias/components/NewsAdBridge'
 import { ClickableCardsBridge } from './shared/behaviors/components/ClickableCardsBridge'
-import { HomePageAdjustmentsBridge } from './pages/home/components/HomePageAdjustmentsBridge'
-import { HomeSidebarAdBridge } from './pages/home/components/HomeSidebarAdBridge'
 import { PublicSearchSuggestionsBridge } from './shared/behaviors/components/PublicSearchSuggestionsBridge'
 import { ColaborePageBridge } from './pages/colabore/components/ColaborePageBridge'
 import './styles/public-styles.css'
@@ -25,7 +22,7 @@ async function waitForPublicFonts() {
 
 function mountApp() {
   ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode><HashRouter><App/><HomeAdBridge/><HeaderBrandBridge/><BrandAssetsBridge/><NewsAdBridge/><ClickableCardsBridge/><HomePageAdjustmentsBridge/><HomeSidebarAdBridge/><PublicSearchSuggestionsBridge/><ColaborePageBridge/></HashRouter></React.StrictMode>,
+    <React.StrictMode><HashRouter><App/><HeaderBrandBridge/><BrandAssetsBridge/><NewsAdBridge/><ClickableCardsBridge/><PublicSearchSuggestionsBridge/><ColaborePageBridge/></HashRouter></React.StrictMode>,
   )
 }
 

13c8b1803e33c98224ecd7925bec8acca3b4ef83
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-28 22:57:24 -0300

    refactor(site-manager): isolate site manager routing

---
+++ src/features/site-manager/SiteManagerRoutes.tsx
@@ -0,0 +1,16 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { SiteCategories, SiteContents, SiteManagerDashboard, SiteMedia, SitePages } from './SiteManagerWorkspace'
+import { MediaKitPage, SiteSettingsPage } from './SiteManagerOperations'
+
+export default function SiteManagerRoutes(){
+  return <Routes>
+    <Route path="/app/site" element={<SiteManagerDashboard/>}/>
+    <Route path="/app/site/conteudos" element={<SiteContents/>}/>
+    <Route path="/app/site/paginas" element={<SitePages/>}/>
+    <Route path="/app/site/categorias" element={<SiteCategories/>}/>
+    <Route path="/app/site/midia" element={<SiteMedia/>}/>
+    <Route path="/app/site/midia-kit" element={<MediaKitPage/>}/>
+    <Route path="/app/site/configuracoes" element={<SiteSettingsPage/>}/>
+    <Route path="*" element={<Navigate to="/app/site" replace/>}/>
+  </Routes>
+}

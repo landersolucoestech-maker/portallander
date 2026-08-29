acd2763e47230df12ee94b3085b1625dd9cc6fe5
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-28 23:09:05 -0300

    refactor(site-manager): route hero manager inside site domain

--- src/features/site-manager/SiteManagerRoutes.tsx
+++ src/features/site-manager/SiteManagerRoutes.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
+import HeroManagerPage from './HeroManagerPage'
 import { SiteContents, SiteManagerDashboard, SitePages } from './SiteManagerWorkspace'
 import { SiteCategories, SiteMedia } from './SiteManagerCatalogPages'
 import { MediaKitPage, SiteSettingsPage } from './SiteManagerOperations'
@@ -6,6 +7,7 @@ import { MediaKitPage, SiteSettingsPage } from './SiteManagerOperations'
 export default function SiteManagerRoutes(){
   return <Routes>
     <Route path="/app/site" element={<SiteManagerDashboard/>}/>
+    <Route path="/app/site/home/hero" element={<HeroManagerPage/>}/>
     <Route path="/app/site/conteudos" element={<SiteContents/>}/>
     <Route path="/app/site/paginas" element={<SitePages/>}/>
     <Route path="/app/site/categorias" element={<SiteCategories/>}/>

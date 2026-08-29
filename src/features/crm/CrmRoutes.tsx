c09469187a524dd8caf03f71bcbd7caee04f32ec
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 12:55:28 -0300

    refactor(crm): route contacts leads and contracts to cloned reference flows

--- src/features/crm/CrmRoutes.tsx
+++ src/features/crm/CrmRoutes.tsx
@@ -1,7 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { OperationsPage } from '../operations/OperationsPage'
-import { ContractTemplatesPage, ContractVariablesPage } from '../operations/ContractRegistryPages'
-import { ContractsPage } from '../operations/ContractsPage'
+import { ContractsReferencePage } from '../operations/ContractsReferencePage'
 import { MarketingReferencePage } from '../operations/MarketingReferencePage'
 import { AgendaReferencePage } from '../operations/AgendaReferencePage'
 import { ReportsReferencePage } from '../operations/ReportsReferencePage'
@@ -9,24 +8,24 @@ import { AccountingReferencePage } from '../operations/AccountingReferencePage'
 import { RHReferencePage } from '../operations/RHReferencePage'
 import { SettingsReferencePage } from '../operations/SettingsReferencePage'
 import { ChatReferencePage } from '../operations/ChatReferencePage'
-import { ContactsPage } from './pages/ContactsPage'
+import { ContactsReferencePage } from './pages/ContactsReferencePage'
 import { CrmDashboardPage } from './pages/CrmDashboardPage'
 import { ReportsPage } from './pages/ReportsPage'
 
 export default function CrmRoutes(){
   return <Routes>
     <Route index element={<CrmDashboardPage/>}/>
-    <Route path="contatos" element={<ContactsPage/>}/>
+    <Route path="contatos" element={<ContactsReferencePage/>}/>
     <Route path="campanhas" element={<Navigate to="/app/crm/marketing/campanhas" replace/>}/>
     <Route path="marketing" element={<Navigate to="/app/crm/marketing/visao-geral" replace/>}/>
     <Route path="marketing/:sectionKey" element={<MarketingReferencePage/>}/>
     <Route path="chat" element={<ChatReferencePage/>}/>
     <Route path="musicchat" element={<Navigate to="/app/crm/chat" replace/>}/>
     <Route path="internal-chat" element={<Navigate to="/app/crm/chat" replace/>}/>
-    <Route path="contracts" element={<ContractsPage/>}/>
-    <Route path="contracts/templates" element={<ContractTemplatesPage/>}/>
-    <Route path="contracts/categorias" element={<Navigate to="/app/crm/contracts" replace/>}/>
-    <Route path="contracts/variaveis" element={<ContractVariablesPage/>}/>
+    <Route path="contracts" element={<ContractsReferencePage initialTab="contracts"/>}/>
+    <Route path="contracts/templates" element={<ContractsReferencePage initialTab="templates"/>}/>
+    <Route path="contracts/categorias" element={<ContractsReferencePage initialTab="categories"/>}/>
+    <Route path="contracts/variaveis" element={<ContractsReferencePage initialTab="variables"/>}/>
     <Route path="events" element={<AgendaReferencePage/>}/>
     <Route path="integrations" element={<Navigate to="/app/crm/settings" replace/>}/>
     <Route path="relatorios" element={<ReportsPage/>}/>

0eaa161b996a2d4d36b431050aa500b0d6b984af
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 01:56:31 -0300

    refactor(crm): remove activities and pipeline routes

--- src/features/crm/CrmRoutes.tsx
+++ src/features/crm/CrmRoutes.tsx
@@ -1,18 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { ActivitiesPage } from './pages/ActivitiesPage'
 import { CampaignsPage } from './pages/CampaignsPage'
 import { ContactsPage } from './pages/ContactsPage'
 import { CrmDashboardPage } from './pages/CrmDashboardPage'
 import { FinancePage } from './pages/FinancePage'
-import { PipelinePage } from './pages/PipelinePage'
 import { ReportsPage } from './pages/ReportsPage'
 
 export default function CrmRoutes(){
   return <Routes>
     <Route index element={<CrmDashboardPage/>}/>
     <Route path="contatos" element={<ContactsPage/>}/>
-    <Route path="atividades" element={<ActivitiesPage/>}/>
-    <Route path="pipeline" element={<PipelinePage/>}/>
     <Route path="campanhas" element={<CampaignsPage/>}/>
     <Route path="relatorios" element={<ReportsPage/>}/>
     <Route path="financeiro" element={<FinancePage/>}/>

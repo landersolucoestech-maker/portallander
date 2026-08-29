31df7410752ad07fc0ea32f1a6108d452fcff860
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-28 23:32:42 -0300

    refactor(crm): scope child routes relatively

--- src/features/crm/CrmRoutes.tsx
+++ src/features/crm/CrmRoutes.tsx
@@ -10,14 +10,14 @@ import { ReportsPage } from './pages/ReportsPage'
 
 export default function CrmRoutes(){
   return <Routes>
-    <Route path="/app/crm" element={<CrmDashboardPage/>}/>
-    <Route path="/app/crm/contatos" element={<ContactsPage/>}/>
-    <Route path="/app/crm/negocios" element={<DealsPage/>}/>
-    <Route path="/app/crm/atividades" element={<ActivitiesPage/>}/>
-    <Route path="/app/crm/pipeline" element={<PipelinePage/>}/>
-    <Route path="/app/crm/campanhas" element={<CampaignsPage/>}/>
-    <Route path="/app/crm/relatorios" element={<ReportsPage/>}/>
-    <Route path="/app/crm/financeiro" element={<FinancePage/>}/>
+    <Route index element={<CrmDashboardPage/>}/>
+    <Route path="contatos" element={<ContactsPage/>}/>
+    <Route path="negocios" element={<DealsPage/>}/>
+    <Route path="atividades" element={<ActivitiesPage/>}/>
+    <Route path="pipeline" element={<PipelinePage/>}/>
+    <Route path="campanhas" element={<CampaignsPage/>}/>
+    <Route path="relatorios" element={<ReportsPage/>}/>
+    <Route path="financeiro" element={<FinancePage/>}/>
     <Route path="*" element={<Navigate to="/app/crm" replace/>}/>
   </Routes>
 }

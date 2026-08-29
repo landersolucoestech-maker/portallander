f4b9a8a4928fad27f0ad8bdbe2bdac6597ba4d7d
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-28 23:32:54 -0300

    refactor(site-manager): scope child routes relatively

--- src/features/site-manager/SiteManagerRoutes.tsx
+++ src/features/site-manager/SiteManagerRoutes.tsx
@@ -10,14 +10,14 @@ import { SiteSettingsPage } from './pages/SiteSettingsPage'
 
 export default function SiteManagerRoutes(){
   return <Routes>
-    <Route path="/app/site" element={<SiteManagerDashboardPage/>}/>
-    <Route path="/app/site/home/hero" element={<HeroManagerPage/>}/>
-    <Route path="/app/site/conteudos" element={<SiteContentsPage/>}/>
-    <Route path="/app/site/paginas" element={<SitePagesPage/>}/>
-    <Route path="/app/site/categorias" element={<SiteCategoriesPage/>}/>
-    <Route path="/app/site/midia" element={<SiteMediaPage/>}/>
-    <Route path="/app/site/midia-kit" element={<MediaKitPage/>}/>
-    <Route path="/app/site/configuracoes" element={<SiteSettingsPage/>}/>
+    <Route index element={<SiteManagerDashboardPage/>}/>
+    <Route path="home/hero" element={<HeroManagerPage/>}/>
+    <Route path="conteudos" element={<SiteContentsPage/>}/>
+    <Route path="paginas" element={<SitePagesPage/>}/>
+    <Route path="categorias" element={<SiteCategoriesPage/>}/>
+    <Route path="midia" element={<SiteMediaPage/>}/>
+    <Route path="midia-kit" element={<MediaKitPage/>}/>
+    <Route path="configuracoes" element={<SiteSettingsPage/>}/>
     <Route path="*" element={<Navigate to="/app/site" replace/>}/>
   </Routes>
 }

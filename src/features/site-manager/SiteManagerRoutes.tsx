5c76989a1a1015e84fe873d0c10389f25a1bf4db
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-28 23:31:31 -0300

    refactor(site-manager): route extracted workspace pages

--- src/features/site-manager/SiteManagerRoutes.tsx
+++ src/features/site-manager/SiteManagerRoutes.tsx
@@ -1,17 +1,19 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import HeroManagerPage from './HeroManagerPage'
-import { SiteContents, SiteManagerDashboard, SitePages } from './SiteManagerWorkspace'
 import { MediaKitPage } from './pages/MediaKitPage'
 import { SiteCategoriesPage } from './pages/SiteCategoriesPage'
+import { SiteContentsPage } from './pages/SiteContentsPage'
+import { SiteManagerDashboardPage } from './pages/SiteManagerDashboardPage'
 import { SiteMediaPage } from './pages/SiteMediaPage'
+import { SitePagesPage } from './pages/SitePagesPage'
 import { SiteSettingsPage } from './pages/SiteSettingsPage'
 
 export default function SiteManagerRoutes(){
   return <Routes>
-    <Route path="/app/site" element={<SiteManagerDashboard/>}/>
+    <Route path="/app/site" element={<SiteManagerDashboardPage/>}/>
     <Route path="/app/site/home/hero" element={<HeroManagerPage/>}/>
-    <Route path="/app/site/conteudos" element={<SiteContents/>}/>
-    <Route path="/app/site/paginas" element={<SitePages/>}/>
+    <Route path="/app/site/conteudos" element={<SiteContentsPage/>}/>
+    <Route path="/app/site/paginas" element={<SitePagesPage/>}/>
     <Route path="/app/site/categorias" element={<SiteCategoriesPage/>}/>
     <Route path="/app/site/midia" element={<SiteMediaPage/>}/>
     <Route path="/app/site/midia-kit" element={<MediaKitPage/>}/>

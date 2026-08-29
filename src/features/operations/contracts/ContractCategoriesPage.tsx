0823c0e3fc31349ae3107937d45eaa17272f26ad
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 13:59:51 -0300

    refactor(contracts): add dedicated categories page

---
+++ src/features/operations/contracts/ContractCategoriesPage.tsx
@@ -0,0 +1,5 @@
+import { ContractsReferencePage } from '../ContractsReferencePage'
+
+export function ContractCategoriesPage(){
+  return <div className="reference-route-separated"><ContractsReferencePage initialTab="categories"/></div>
+}

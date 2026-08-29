90122303cba9b2b5b61eed21fc70c15283ea1d75
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-28 23:51:26 -0300

    refactor(branding): remove header brand bridge from bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './app/PortalApp'
-import { HeaderBrandBridge } from './shared/branding/components/HeaderBrandBridge'
 import { BrandAssetsBridge } from './shared/branding/components/BrandAssetsBridge'
 import { NewsAdBridge } from './pages/noticias/components/NewsAdBridge'
 import { ClickableCardsBridge } from './shared/behaviors/components/ClickableCardsBridge'
@@ -21,7 +20,7 @@ async function waitForPublicFonts() {
 
 function mountApp() {
   ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode><HashRouter><App/><HeaderBrandBridge/><BrandAssetsBridge/><NewsAdBridge/><ClickableCardsBridge/><PublicSearchSuggestionsBridge/></HashRouter></React.StrictMode>,
+    <React.StrictMode><HashRouter><App/><BrandAssetsBridge/><NewsAdBridge/><ClickableCardsBridge/><PublicSearchSuggestionsBridge/></HashRouter></React.StrictMode>,
   )
 }
 

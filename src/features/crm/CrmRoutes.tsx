39f3ab7e4de7c7d7097d11846d4e2e4e31db2357
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 01:51:42 -0300

    refactor(crm): remove deals route

--- src/features/crm/CrmRoutes.tsx
+++ src/features/crm/CrmRoutes.tsx
@@ -3,7 +3,6 @@ import { ActivitiesPage } from './pages/ActivitiesPage'
 import { CampaignsPage } from './pages/CampaignsPage'
 import { ContactsPage } from './pages/ContactsPage'
 import { CrmDashboardPage } from './pages/CrmDashboardPage'
-import { DealsPage } from './pages/DealsPage'
 import { FinancePage } from './pages/FinancePage'
 import { PipelinePage } from './pages/PipelinePage'
 import { ReportsPage } from './pages/ReportsPage'
@@ -12,7 +11,6 @@ export default function CrmRoutes(){
   return <Routes>
     <Route index element={<CrmDashboardPage/>}/>
     <Route path="contatos" element={<ContactsPage/>}/>
-    <Route path="negocios" element={<DealsPage/>}/>
     <Route path="atividades" element={<ActivitiesPage/>}/>
     <Route path="pipeline" element={<PipelinePage/>}/>
     <Route path="campanhas" element={<CampaignsPage/>}/>

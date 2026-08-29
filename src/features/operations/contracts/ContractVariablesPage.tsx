974a0f766c2e30e5717d5b1206efea213b3e75c2
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 13:59:44 -0300

    refactor(contracts): add dedicated variables page

---
+++ src/features/operations/contracts/ContractVariablesPage.tsx
@@ -0,0 +1,5 @@
+import { ContractsReferencePage } from '../ContractsReferencePage'
+
+export function ContractVariablesPage(){
+  return <div className="reference-route-separated"><ContractsReferencePage initialTab="variables"/></div>
+}

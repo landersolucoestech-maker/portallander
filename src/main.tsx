40a993d58779f601b852b96221daa9135cc04b31
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 16:20:38 -0300

    feat(dashboard): provide TanStack Query runtime

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { HashRouter } from 'react-router-dom'
 import App from './app/PortalApp'
 import './styles/public-styles.css'
+
+const queryClient=new QueryClient()
 
 const REQUIRED_PUBLIC_FONTS = [
   '400 16px "Bebas Neue"','400 16px "Montserrat"','500 16px "Montserrat"','600 16px "Montserrat"','700 16px "Montserrat"','800 16px "Montserrat"',
@@ -16,7 +19,7 @@ async function waitForPublicFonts() {
 
 function mountApp() {
   ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode><HashRouter><App/></HashRouter></React.StrictMode>,
+    <React.StrictMode><QueryClientProvider client={queryClient}><HashRouter><App/></HashRouter></QueryClientProvider></React.StrictMode>,
   )
 }
 

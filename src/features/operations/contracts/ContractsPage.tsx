5f5475935f0c6124872a1fb56db1c7f715dcb799
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 13:59:21 -0300

    refactor(contracts): add dedicated contracts route page

---
+++ src/features/operations/contracts/ContractsPage.tsx
@@ -0,0 +1,5 @@
+import { ContractsReferencePage } from '../ContractsReferencePage'
+
+export function ContractsPage(){
+  return <div className="reference-route-separated"><ContractsReferencePage initialTab="contracts"/></div>
+}

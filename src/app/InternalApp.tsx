35a7a181b460da7d4a1097ab26db67ef32a2917f
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 03:13:23 -0300

    fix(app): remove standalone operations workspace route

--- src/app/InternalApp.tsx
+++ src/app/InternalApp.tsx
@@ -2,7 +2,6 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../features/access/LoginPage'
 import { WorkspacePage } from '../features/access/WorkspacePage'
 import CrmRoutes from '../features/crm/CrmRoutes'
-import OperationsRoutes from '../features/operations/OperationsRoutes'
 import SiteManagerRoutes from '../features/site-manager/SiteManagerRoutes'
 import '../styles/admin-entry.css'
 
@@ -12,7 +11,6 @@ export default function InternalApp(){
     <Route path="/app/login" element={<LoginPage/>}/>
     <Route path="/app/workspaces" element={<WorkspacePage/>}/>
     <Route path="/app/crm/*" element={<CrmRoutes/>}/>
-    <Route path="/app/operations/*" element={<OperationsRoutes/>}/>
     <Route path="/app/site/*" element={<SiteManagerRoutes/>}/>
     <Route path="*" element={<Navigate to="/app/login" replace/>}/>
   </Routes>

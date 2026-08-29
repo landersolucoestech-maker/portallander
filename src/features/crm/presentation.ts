1c5a11caf0e609488973edf57047add914c28b85
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 15:54:39 -0300

    refactor: remove crm presentation

--- src/features/crm/presentation.ts
+++
@@ -1,1 +0,0 @@
-export const CRM_DEMO_DESCRIPTION='Os registros abaixo são demonstrativos e servem somente para validar a estrutura visual e operacional do CRM do Portal Lander. Nenhuma alteração é persistida.'

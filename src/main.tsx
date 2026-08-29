6b39f3de54dcbfe50c6fdef1b571c0aeb4ca93c0
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-28 23:55:06 -0300

    refactor(public): remove clickable cards bridge from bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './app/PortalApp'
 import { NewsAdBridge } from './pages/noticias/components/NewsAdBridge'
-import { ClickableCardsBridge } from './shared/behaviors/components/ClickableCardsBridge'
 import { PublicSearchSuggestionsBridge } from './shared/behaviors/components/PublicSearchSuggestionsBridge'
 import './styles/public-styles.css'
 
@@ -19,7 +18,7 @@ async function waitForPublicFonts() {
 
 function mountApp() {
   ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode><HashRouter><App/><NewsAdBridge/><ClickableCardsBridge/><PublicSearchSuggestionsBridge/></HashRouter></React.StrictMode>,
+    <React.StrictMode><HashRouter><App/><NewsAdBridge/><PublicSearchSuggestionsBridge/></HashRouter></React.StrictMode>,
   )
 }
 

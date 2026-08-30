5e9103f726e561ed72a1b3b3dd5a6f4700165844
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-30 12:57:14 -0300

    refactor(header): move site pages header into shared shell

--- apps/web/src/features/site-manager/pages/SitePagesPage.tsx
+++ apps/web/src/features/site-manager/pages/SitePagesPage.tsx
@@ -1,7 +1,8 @@
+import { ADMIN_CAPABILITIES } from '../../../shared/internal/adminCapabilities'
 import { SITE_MANAGER_NAV } from '../../../shared/internal/adminNavigation'
 import { AdminShell } from '../../../shared/internal/AdminUi'
 import { EditorialPagesAdmin } from '../../editorial/components/EditorialAdmin'
 
 export function SitePagesPage(){
-  return <AdminShell area="cms" items={SITE_MANAGER_NAV}><EditorialPagesAdmin/></AdminShell>
+  return <AdminShell area="cms" items={SITE_MANAGER_NAV} header={{title:'Páginas',description:'Estrutura editorial, navegação, publicação e relacionamento entre páginas do portal.'}} headerAction={{label:'Nova página',disabled:true,disabledReason:ADMIN_CAPABILITIES.editorialPersistence.description}}><EditorialPagesAdmin/></AdminShell>
 }

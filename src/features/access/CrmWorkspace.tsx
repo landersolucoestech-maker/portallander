c70d42ce25dc62349055b0b4f8d88d80e82f4d80
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 21:17:21 -0300

    feat(crm): route leads and contacts

--- src/features/access/CrmWorkspace.tsx
+++ src/features/access/CrmWorkspace.tsx
@@ -1,9 +1,12 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import {Navigate,Route,Routes} from 'react-router-dom'
 import DashboardPage from '../dashboard/DashboardPage'
+import CrmPage from '../crm/CrmPage'
 
 export default function CrmWorkspace(){
-  return <Routes>
-    <Route index element={<DashboardPage/>}/>
-    <Route path="*" element={<Navigate to="/app/crm" replace/>}/>
-  </Routes>
+ return <Routes>
+  <Route index element={<DashboardPage/>}/>
+  <Route path="leads" element={<CrmPage view="leads"/>}/>
+  <Route path="contatos" element={<CrmPage view="contacts"/>}/>
+  <Route path="*" element={<Navigate to="/app/crm" replace/>}/>
+ </Routes>
 }

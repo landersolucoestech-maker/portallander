e7485acc089311657a0d76a98478bf8b693f5e82
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-30 12:57:22 -0300

    refactor(header): move content header into shared shell

--- apps/web/src/features/site-manager/pages/SiteContentsPage.tsx
+++ apps/web/src/features/site-manager/pages/SiteContentsPage.tsx
@@ -1,7 +1,8 @@
+import { ADMIN_CAPABILITIES } from '../../../shared/internal/adminCapabilities'
 import { SITE_MANAGER_NAV } from '../../../shared/internal/adminNavigation'
 import { AdminShell } from '../../../shared/internal/AdminUi'
 import { EditorialContentsAdmin } from '../../editorial/components/EditorialAdmin'
 
 export function SiteContentsPage(){
-  return <AdminShell area="cms" items={SITE_MANAGER_NAV}><EditorialContentsAdmin/></AdminShell>
+  return <AdminShell area="cms" items={SITE_MANAGER_NAV} header={{title:'Conteúdos',description:'Gerencie todos os conteúdos editoriais por uma única estrutura, independentemente da página ou categoria.'}} headerAction={{label:'Novo conteúdo',disabled:true,disabledReason:ADMIN_CAPABILITIES.editorialPersistence.description}}><EditorialContentsAdmin/></AdminShell>
 }

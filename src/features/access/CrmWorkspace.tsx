daac5ca9c2c48212d575c57c168f24ed74702384
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 21:29:07 -0300

    refactor(crm): unify contacts and leads in one page

--- src/features/access/CrmWorkspace.tsx
+++ src/features/access/CrmWorkspace.tsx
@@ -1,12 +1,11 @@
 import {Navigate,Route,Routes} from 'react-router-dom'
-import DashboardPage from '../dashboard/DashboardPage'
 import CrmPage from '../crm/CrmPage'
 
 export default function CrmWorkspace(){
  return <Routes>
-  <Route index element={<DashboardPage/>}/>
-  <Route path="leads" element={<CrmPage view="leads"/>}/>
-  <Route path="contatos" element={<CrmPage view="contacts"/>}/>
+  <Route index element={<CrmPage/>}/>
+  <Route path="contatos" element={<CrmPage/>}/>
+  <Route path="leads" element={<CrmPage/>}/>
   <Route path="*" element={<Navigate to="/app/crm" replace/>}/>
  </Routes>
 }

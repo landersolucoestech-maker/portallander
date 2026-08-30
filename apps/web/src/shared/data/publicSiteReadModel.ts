817f733559fbf358392d2c3d0cb20dd3326ae26e
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-30 14:08:25 -0300

    feat(data): expose public site domain read model

---
+++ apps/web/src/shared/data/publicSiteReadModel.ts
@@ -0,0 +1,9 @@
+import {getRuntimeDataProvider} from './runtimeDataProvider'
+
+export const publicSiteReadModel={
+ collaborationTypes(){return getRuntimeDataProvider().collaboration.types().filter(item=>item.active)},
+ collaborationGuidelines(){return [...getRuntimeDataProvider().collaboration.guidelines()].sort((a,b)=>a.order-b.order)},
+ socialChannels(){return getRuntimeDataProvider().branding.socialChannels().filter(item=>item.active).sort((a,b)=>a.order-b.order)},
+ branding(){return getRuntimeDataProvider().branding.config()},
+ advertisingCampaigns(){return getRuntimeDataProvider().advertising.campaigns().filter(item=>item.active)},
+}

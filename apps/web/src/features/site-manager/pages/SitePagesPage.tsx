bf4abcb43629a281cd933dab62a2532e1fd69896
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-01 04:27:21 -0300

    refactor(cms): remover módulo administrativo Páginas

--- apps/web/src/features/site-manager/pages/SitePagesPage.tsx
+++
@@ -1,8 +0,0 @@
-import { ADMIN_CAPABILITIES } from '../../../shared/internal/adminCapabilities'
-import { SITE_MANAGER_NAV } from '../../../shared/internal/adminNavigation'
-import { AdminShell } from '../../../shared/internal/AdminUi'
-import { EditorialPagesAdmin } from '../../editorial/components/EditorialAdmin'
-
-export function SitePagesPage(){
-  return <AdminShell area="cms" items={SITE_MANAGER_NAV} header={{title:'Páginas',description:'Estrutura editorial, navegação, publicação e relacionamento entre páginas do portal.'}} headerAction={{label:'Nova página',disabled:true,disabledReason:ADMIN_CAPABILITIES.editorialPersistence.description}}><EditorialPagesAdmin/></AdminShell>
-}

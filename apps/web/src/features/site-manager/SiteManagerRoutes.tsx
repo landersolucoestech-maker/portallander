8c06ff4f3eea543eb0f6c989dec94fa3dfcc7077
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-01 04:26:00 -0300

    refactor(cms): remover rotas dos módulos administrativos excluídos

--- apps/web/src/features/site-manager/SiteManagerRoutes.tsx
+++ apps/web/src/features/site-manager/SiteManagerRoutes.tsx
@@ -1,15 +1,10 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { BrandAssetsManagerPage } from './pages/BrandAssetsManagerPage'
 import { FooterSectionManagerPage } from './pages/FooterSectionManagerPage'
-import { HeaderBrandManagerPage } from './pages/HeaderBrandManagerPage'
 import { HeroSectionAppearancePage } from './pages/HeroSectionAppearancePage'
 import { MediaKitPage } from './pages/MediaKitPage'
-import { NewsAdManagerPage } from './pages/NewsAdManagerPage'
-import { SiteCategoriesPage } from './pages/SiteCategoriesPage'
 import { SiteContentsPage } from './pages/SiteContentsPage'
 import { SiteManagerDashboardPage } from './pages/SiteManagerDashboardPage'
 import { SiteMediaPage } from './pages/SiteMediaPage'
-import { SitePagesPage } from './pages/SitePagesPage'
 import { SiteSectionsPage } from './pages/SiteSectionsPage'
 
 export default function SiteManagerRoutes(){
@@ -18,19 +13,13 @@ export default function SiteManagerRoutes(){
 
     <Route path="home" element={<Navigate to="/app/site/secoes" replace/>}/>
     <Route path="home/hero" element={<Navigate to="/app/site/secoes/home/hero" replace/>}/>
-    <Route path="home/anuncio" element={<Navigate to="/app/site/noticias/anuncio" replace/>}/>
 
     <Route path="secoes" element={<SiteSectionsPage/>}/>
     <Route path="secoes/home/hero" element={<HeroSectionAppearancePage/>}/>
     <Route path="secoes/home/rodape" element={<FooterSectionManagerPage/>}/>
 
-    <Route path="marca" element={<BrandAssetsManagerPage/>}/>
-    <Route path="cabecalho" element={<HeaderBrandManagerPage/>}/>
     <Route path="conteudos" element={<SiteContentsPage/>}/>
-    <Route path="paginas" element={<SitePagesPage/>}/>
-    <Route path="categorias" element={<SiteCategoriesPage/>}/>
     <Route path="midia" element={<SiteMediaPage/>}/>
-    <Route path="noticias/anuncio" element={<NewsAdManagerPage/>}/>
     <Route path="midia-kit" element={<MediaKitPage/>}/>
     <Route path="*" element={<Navigate to="/app/site" replace/>}/>
   </Routes>

818fc0f91cd009df579f1728e41b029a0ad2ad6e
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-02 17:50:06 -0300

    feat(site): route Mais Lidas to dedicated configurator

--- apps/web/src/features/site-manager/SiteManagerRoutes.tsx
+++ apps/web/src/features/site-manager/SiteManagerRoutes.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { MediaKitPage } from './pages/MediaKitPage'
 import { GlobalHeroEditorPage } from './pages/GlobalHeroEditorPage'
 import { HomeAdvertisingSectionPage } from './pages/HomeAdvertisingSectionPage'
+import { HomeMostReadSectionPage } from './pages/HomeMostReadSectionPage'
 import { SectionConfigurationPage } from './pages/SectionConfigurationPage'
 import { SiteCollaborationsPage } from './pages/SiteCollaborationsPage'
 import { SiteContentEditorPage } from './pages/SiteContentEditorPage'
@@ -21,6 +22,7 @@ export default function SiteManagerRoutes(){
     <Route path="paginas/home/hero" element={<Navigate to="/app/site/paginas/home/secoes/hero" replace/>}/>
     <Route path="paginas/home/secoes/publicidade-lateral" element={<HomeAdvertisingSectionPage sectionId="publicidade-lateral"/>}/>
     <Route path="paginas/home/secoes/anuncie-aqui" element={<HomeAdvertisingSectionPage sectionId="anuncie-aqui"/>}/>
+    <Route path="paginas/home/secoes/mais-lidas" element={<HomeMostReadSectionPage/>}/>
     <Route path="paginas/:pageId/secoes/editorial-hero" element={<GlobalHeroEditorPage sectionId="editorial-hero"/>}/>
     <Route path="paginas/:pageId/secoes/sobre-hero" element={<GlobalHeroEditorPage sectionId="sobre-hero"/>}/>
     <Route path="paginas/:pageId/secoes/contato-hero" element={<GlobalHeroEditorPage sectionId="contato-hero"/>}/>

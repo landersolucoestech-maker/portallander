c407ee226abc8a63a817b3b00c252bd38bde200a
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-01 21:30:55 -0300

    feat(site): route forms and collaborations

--- apps/web/src/features/site-manager/SiteManagerRoutes.tsx
+++ apps/web/src/features/site-manager/SiteManagerRoutes.tsx
@@ -1,7 +1,9 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { HeroSectionAppearancePage } from './pages/HeroSectionAppearancePage'
 import { MediaKitPage } from './pages/MediaKitPage'
+import { SiteCollaborationsPage } from './pages/SiteCollaborationsPage'
 import { SiteContentsPage } from './pages/SiteContentsPage'
+import { SiteFormsPage } from './pages/SiteFormsPage'
 import { SiteManagerDashboardPage } from './pages/SiteManagerDashboardPage'
 import { SiteMediaPage } from './pages/SiteMediaPage'
 import { SiteSectionsPage } from './pages/SiteSectionsPage'
@@ -25,6 +27,8 @@ export default function SiteManagerRoutes(){
     <Route path="rodape" element={<Navigate to="/app/site/configuracoes" replace/>}/>
     <Route path="configuracoes" element={<SiteSettingsPage/>}/>
     <Route path="conteudos" element={<SiteContentsPage/>}/>
+    <Route path="conteudos/colaboracoes" element={<SiteCollaborationsPage/>}/>
+    <Route path="formularios" element={<SiteFormsPage/>}/>
     <Route path="midia" element={<SiteMediaPage/>}/>
     <Route path="midia-kit" element={<MediaKitPage/>}/>
     <Route path="*" element={<Navigate to="/app/site" replace/>}/>

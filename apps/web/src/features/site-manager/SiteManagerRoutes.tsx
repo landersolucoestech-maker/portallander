c4d7ebd922718c99972ef88f10174152c82c4c8f
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-01 22:45:50 -0300

    refactor(site): move global identity out of site routes

--- apps/web/src/features/site-manager/SiteManagerRoutes.tsx
+++ apps/web/src/features/site-manager/SiteManagerRoutes.tsx
@@ -8,25 +8,20 @@ import { SiteFormsPage } from './pages/SiteFormsPage'
 import { SiteManagerDashboardPage } from './pages/SiteManagerDashboardPage'
 import { SiteMediaPage } from './pages/SiteMediaPage'
 import { SiteSectionsPage } from './pages/SiteSectionsPage'
-import { SiteSettingsPage } from './pages/SiteSettingsPage'
 
 export default function SiteManagerRoutes(){
   return <Routes>
     <Route index element={<SiteManagerDashboardPage/>}/>
-
     <Route path="home" element={<Navigate to="/app/site/paginas" replace/>}/>
     <Route path="home/hero" element={<Navigate to="/app/site/paginas/home/hero" replace/>}/>
-
     <Route path="paginas" element={<SiteSectionsPage/>}/>
     <Route path="paginas/home/hero" element={<HeroSectionAppearancePage/>}/>
-
     <Route path="secoes" element={<Navigate to="/app/site/paginas" replace/>}/>
     <Route path="secoes/home/hero" element={<Navigate to="/app/site/paginas/home/hero" replace/>}/>
-    <Route path="secoes/home/rodape" element={<Navigate to="/app/site/configuracoes" replace/>}/>
-
-    <Route path="cabecalho" element={<Navigate to="/app/site/configuracoes" replace/>}/>
-    <Route path="rodape" element={<Navigate to="/app/site/configuracoes" replace/>}/>
-    <Route path="configuracoes" element={<SiteSettingsPage/>}/>
+    <Route path="secoes/home/rodape" element={<Navigate to="/app/settings" replace/>}/>
+    <Route path="cabecalho" element={<Navigate to="/app/settings" replace/>}/>
+    <Route path="rodape" element={<Navigate to="/app/settings" replace/>}/>
+    <Route path="configuracoes" element={<Navigate to="/app/settings" replace/>}/>
     <Route path="conteudos" element={<SiteContentsPage/>}/>
     <Route path="conteudos/colaboracoes" element={<SiteCollaborationsPage/>}/>
     <Route path="formularios" element={<SiteFormsPage/>}/>

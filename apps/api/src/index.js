65bb90df2573f6048bcd0894228d784efcf6aadf
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-02 23:44:11 -0300

    feat(spotify): wire release integration HTTP handler

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -2,10 +2,12 @@ import {createServer} from 'node:http'
 import {handleRequest} from './http.js'
 import {handlePageSectionRequest} from './pageSectionHttp.js'
 import {handleSectionConfigurationRequest} from './sectionConfigurationHttp.js'
+import {handleSpotifyReleaseRequest} from './spotifyReleaseHttp.js'
 
 const port=Number(process.env.PORT||8787)
 const server=createServer((req,res)=>{
   void (async()=>{
+    if(await handleSpotifyReleaseRequest(req,res))return
     if(await handleSectionConfigurationRequest(req,res))return
     if(await handlePageSectionRequest(req,res))return
     await handleRequest(req,res)

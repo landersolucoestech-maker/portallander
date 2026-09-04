6bb7eb65e87f764e78402d6f080bd8fbdcc66574
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-04 19:19:27 -0300

    feat(finance): register authenticated finance API

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -4,6 +4,7 @@ import {handleAgenticRequest} from './agenticHttp.js'
 import {handleAgendaRequest} from './agendaHttp.js'
 import {handleAnalyticsRequest} from './analyticsHttp.js'
 import {handleCrmRequest} from './crmHttp.js'
+import {handleFinanceRequest} from './financeHttp.js'
 import {handleIntegrationProviderRequest} from './integrationProviderHttp.js'
 import {handleNewsletterRequest} from './newsletterHttp.js'
 import {handlePageSectionRequest} from './pageSectionHttp.js'
@@ -17,6 +18,7 @@ const server=createServer((req,res)=>{
     if(await handleAgendaRequest(req,res))return
     if(await handleAnalyticsRequest(req,res))return
     if(await handleCrmRequest(req,res))return
+    if(await handleFinanceRequest(req,res))return
     if(await handleIntegrationProviderRequest(req,res))return
     if(await handleNewsletterRequest(req,res))return
     if(await handleSpotifyReleaseRequest(req,res))return

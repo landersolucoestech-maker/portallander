a1f7df03233e2e353fbe531e530ae17a33bcdd19
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-04 17:59:27 -0300

    feat(crm): register authenticated CRM API

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -2,6 +2,7 @@ import {createServer} from 'node:http'
 import {handleRequest} from './http.js'
 import {handleAgenticRequest} from './agenticHttp.js'
 import {handleAnalyticsRequest} from './analyticsHttp.js'
+import {handleCrmRequest} from './crmHttp.js'
 import {handleIntegrationProviderRequest} from './integrationProviderHttp.js'
 import {handleNewsletterRequest} from './newsletterHttp.js'
 import {handlePageSectionRequest} from './pageSectionHttp.js'
@@ -13,6 +14,7 @@ const server=createServer((req,res)=>{
   void (async()=>{
     if(await handleAgenticRequest(req,res))return
     if(await handleAnalyticsRequest(req,res))return
+    if(await handleCrmRequest(req,res))return
     if(await handleIntegrationProviderRequest(req,res))return
     if(await handleNewsletterRequest(req,res))return
     if(await handleSpotifyReleaseRequest(req,res))return

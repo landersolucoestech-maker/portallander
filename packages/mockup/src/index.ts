6e8cf356a83b73113e824c46f9b4329cabd7715e
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-06 03:17:25 -0300

    fix(forms): export canonical development forms

--- packages/mockup/src/index.ts
+++ packages/mockup/src/index.ts
@@ -1,3 +1,5 @@
 export {getMockupScenario,mockupRegistry} from './registry'
 export type {MockupScenario,MockupScenarioName} from './registry'
+export {getMockupSystemForms} from './forms'
+export type {MockupSystemForm} from './forms'
 export * from './generated/runtimeExports'
